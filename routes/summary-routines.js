15f9094ca2230bb51c0d577bd8b5631a93e8a0de
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-11-25 23:52:12 +0100

    summary route

---
+++ routes/summary-routines.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+router.get('/', function(req, res, next) {
+    // Ange sökvägarna till mapparna "opening", "closing", och "summary"
+    var openingFolderPath = path.join(__dirname, '..', 'routines', 'opening');
+    var closingFolderPath = path.join(__dirname, '..', 'routines', 'closing');
+    var summaryFolderPath = path.join(__dirname, '..', 'routines', 'summary');
+
+    // Skapa "summary"-mappen om den inte redan finns
+    if (!fs.existsSync(summaryFolderPath)) {
+        fs.mkdirSync(summaryFolderPath);
+    }
+
+    try {
+        // Hämta alla filnamn från "opening" och "closing"
+        var openingFiles = fs.readdirSync(openingFolderPath);
+        var closingFiles = fs.readdirSync(closingFolderPath);
+
+        // Loopa igenom och kopiera filerna till "summary"-mappen
+        openingFiles.forEach(function(file) {
+            var sourcePath = path.join(openingFolderPath, file);
+            var destinationPath = path.join(summaryFolderPath, file);
+            fs.copyFileSync(sourcePath, destinationPath);
+        });
+
+        closingFiles.forEach(function(file) {
+            var sourcePath = path.join(closingFolderPath, file);
+            var destinationPath = path.join(summaryFolderPath, file);
+            fs.copyFileSync(sourcePath, destinationPath);
+        });
+
+        res.json({ success: true, message: 'Routines copied to summary folder' });
+    } catch (error) {
+        console.log('Error copying routines to summary folder', error);
+        res.status(500).json({ error: 'Failed to copy routines to summary folder' });
+    }
+});
+
+module.exports = router;
