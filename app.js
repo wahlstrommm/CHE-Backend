3c1dfeaf7f50c7930efe950d86e27cd3b58259ab
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-11-26 21:54:40 +0100

    inplamented cors

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var openingRoutines = require('./routes/opening-routines');
-var closingRoutines = require('./routes/closing-routines');
-var summaryRoutines = require('./routes/summary-routines');
-var uploadRoute = require('./routes/upload');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var openingRoutines = require("./routes/opening-routines");
+var closingRoutines = require("./routes/closing-routines");
+var summaryRoutines = require("./routes/summary-routines");
+var uploadRoute = require("./routes/upload");
 var app = express();
-
+var cors = require("cors");
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/opening-routines', openingRoutines);
-app.use('/closing-routines', closingRoutines);
-app.use('/summary', summaryRoutines);
-app.use('/upload',uploadRoute);
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/opening-routines", openingRoutines);
+app.use("/closing-routines", closingRoutines);
+app.use("/summary", summaryRoutines);
+app.use("/upload", uploadRoute);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
