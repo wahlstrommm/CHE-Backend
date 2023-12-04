dd7bcded2f1b1569c02d918102fcc88b4ac0f36e
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-04 20:41:09 +0100

    Imported path

--- routes/special-routines.js
+++ routes/special-routines.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const fs = require("fs");
+const path = require("path");
+
 router.get(("/", function (req, res, next) {}));
 router.post(("/", function (req, res, next) {}));
 module.exports = router;
