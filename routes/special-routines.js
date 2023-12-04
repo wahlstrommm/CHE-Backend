9b85043d5d453a32c96e99c6e5987c89c96711bc
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-04 20:46:29 +0100

    if statment for the checks if file exists

--- routes/special-routines.js
+++ routes/special-routines.js
@@ -10,6 +10,18 @@ router.get(
     var fileName = today.toISOString().split("T")[0] + "-special" + ".json";
     var specialFolderPath = path.join(__dirname, "..", "routines", "special");
     var specialFilePath = path.join(specialFolderPath, fileName);
+    if (fs.existsSync(specialFilePath)) {
+      var fileContent = fs.readFileSync(specialFilePath, "utf-8");
+      try {
+        // Försök att konvertera filens innehåll till ett JSON-objekt
+        var jsonData = JSON.parse(fileContent);
+        res.json(jsonData);
+      } catch (error) {
+        console.log("Error parsing file content", error);
+        res.status(500).json({ error: "Failed to parse file content" });
+      }
+    } else {
+    }
   })
 );
 router.post(("/", function (req, res, next) {}));
