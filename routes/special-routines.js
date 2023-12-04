c8f448fd19d55d4911479d673815d9f10c04bb51
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-04 20:40:52 +0100

    Imported fs

--- routes/special-routines.js
+++ routes/special-routines.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const fs = require("fs");
 router.get(("/", function (req, res, next) {}));
 router.post(("/", function (req, res, next) {}));
 module.exports = router;
