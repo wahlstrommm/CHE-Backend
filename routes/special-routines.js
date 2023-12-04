203de4d09251b181470232f3ba018ead14731fc9
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-04 20:43:52 +0100

    special folder path

--- routes/special-routines.js
+++ routes/special-routines.js
@@ -8,7 +8,8 @@ router.get(
   function (req, res, next) {
     var today = new Date();
     var fileName = today.toISOString().split("T")[0] + "-special" + ".json";
-    var specialFolderPath = path.join(__dirname, "..", "routines", "opening");
+    var specialFolderPath = path.join(__dirname, "..", "routines", "special");
+    var specialFilePath = path.join(specialFolderPath, fileName);
   })
 );
 router.post(("/", function (req, res, next) {}));
