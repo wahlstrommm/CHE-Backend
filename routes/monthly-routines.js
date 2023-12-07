d6b8912ebe683568cbb1ae4ee2b3c3caed55ac09
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-07 23:28:35 +0100

    module export

--- routes/monthly-routines.js
+++ routes/monthly-routines.js
@@ -2,3 +2,6 @@ var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 const path = require("path");
+
+router.post(("/", function (req, res, next) {}));
+module.exports = router;
