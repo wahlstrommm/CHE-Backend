e7e376cc7200c7fe80d6ea3f9589836a64636463
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-07 23:29:04 +0100

    router get parh

--- routes/monthly-routines.js
+++ routes/monthly-routines.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
+router.get(("/", function (req, res, next) {}));
 router.post(("/", function (req, res, next) {}));
 module.exports = router;
