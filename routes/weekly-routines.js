619e6588547c490e997c50a7ff0e3d6da89fde9e
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-09 22:08:11 +0100

    getWeek function

--- routes/weekly-routines.js
+++ routes/weekly-routines.js
@@ -52,5 +52,15 @@ router.get("/", function (req, res, next) {
     }
   }
 });
+
+function getWeek(date) {
+  var d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+  var yearStart = new Date(d.getFullYear(), 0, 1);
+  var weekNumber = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+  return weekNumber;
+}
+
 router.post(("/", function (req, res, next) {}));
 module.exports = router;
