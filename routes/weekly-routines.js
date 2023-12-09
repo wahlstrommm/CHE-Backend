f807fc6a2e32c4e126afab89325508ff9812ddd2
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-09 22:10:03 +0100

    getWeek function

--- routes/weekly-routines.js
+++ routes/weekly-routines.js
@@ -3,6 +3,15 @@ var router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
+function getWeek(date) {
+  var d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+  var yearStart = new Date(d.getFullYear(), 0, 1);
+  var weekNumber = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+  return weekNumber;
+}
+
 router.get("/", function (req, res, next) {
   var today = new Date();
   var fileName = today.toISOString().split("T")[0] + "-weekly" + ".json";
@@ -52,15 +61,6 @@ router.get("/", function (req, res, next) {
     }
   }
 });
-
-function getWeek(date) {
-  var d = new Date(date);
-  d.setHours(0, 0, 0, 0);
-  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
-  var yearStart = new Date(d.getFullYear(), 0, 1);
-  var weekNumber = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-  return weekNumber;
-}
 
 router.post(
   ("/",
