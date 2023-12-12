3fe831069f812b6d9556a3e789c0cf955706f060
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-12 21:24:17 +0100

    Update the get and removed comments

--- routes/weekly-routines.js
+++ routes/weekly-routines.js
@@ -52,80 +52,13 @@ router.get("/", async function (req, res, next) {
     if (latestWeeklyData) {
       res.json(latestWeeklyData);
     } else {
-      await readDataFromFilePath(weeklyFilePath, res, today);
+      const templateData = require("../routines/template/weekly.json");
+      res.json(templateData);
     }
   } catch (error) {
     console.log("Error:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
 module.exports = router;
-
-/*
-router.get("/", function (req, res, next) {
-  var today = new Date();
-  var fileName = today.toISOString().split("T")[0] + "-weekly" + ".json";
-
-  // Ange sökvägen till mappen "weekly"
-  var weeklyFolderPath = path.join(__dirname, "..", "routines", "weekly");
-  var weeklyFilePath = path.join(weeklyFolderPath, fileName);
-
-  // Kontrollera om filen för dagens datum finns i "weekly"
-  if (fs.existsSync(weeklyFilePath)) {
-    // Läs innehållet från den befintliga filen i "weekly"
-    var fileContent = fs.readFileSync(weeklyFilePath, "utf-8");
-    try {
-      // Försök att konvertera filens innehåll till ett JSON-objekt
-      var jsonData = JSON.parse(fileContent);
-      res.json(jsonData);
-    } catch (error) {
-      console.log("Error parsing file content", error);
-      res.status(500).json({ error: "Failed to parse file content" });
-    }
-  } else {
-    // Filen för dagens datum finns inte i "weekly", hämta från "template"
-    var templateFilePath = path.join(
-      __dirname,
-      "..",
-      "routines",
-      "template",
-      fileName
-    );
-
-    if (fs.existsSync(templateFilePath)) {
-      // Läs innehållet från den befintliga filen i "template"
-      var templateFileContent = fs.readFileSync(templateFilePath, "utf-8");
-      try {
-        // Försök att konvertera filens innehåll till ett JSON-objekt
-        var templateJsonData = JSON.parse(templateFileContent);
-        res.json(templateJsonData);
-      } catch (error) {
-        console.log("Error parsing template file content", error);
-        res
-          .status(500)
-          .json({ error: "Failed to parse template file content" });
-      }
-    } else {
-      // Filen för dagens datum finns inte i "template" heller, skicka innehållet från weekly.json
-      res.json(require("../routines/template/weekly.json"));
-    }
-  }
-});
-*/
-
-/* 
-router.post(
-  ("/",
-  function (req, res, next) {
-    var today = new Date();
-    var weekNumber = getWeek(today);
-
-    // Använd veckonumret i filnamnet
-    var fileName =
-      today.toISOString().split("T")[0].slice(0, 7) +
-      "-weekly-" +
-      weekNumber +
-      ".json";
-  })
-);
-*/
