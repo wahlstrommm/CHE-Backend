e66a7733e5e596f53a708e8785f467125d632b0b
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-31 01:35:23 +0100

    Updated the summary

--- routes/summary-routines.js
+++ routes/summary-routines.js
@@ -12,6 +12,7 @@ function readJSONFile(filePath) {
     return null;
   }
 }
+
 function writeJSONFile(filePath, data) {
   try {
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf-8");
@@ -21,118 +22,57 @@ function writeJSONFile(filePath, data) {
   }
 }
 
-// Funktion för att gruppera rutiner efter månad och vecka
-function groupRoutinesByMonthAndWeek(routines) {
-  // Skapa ett objekt för att hålla den grupperade informationen
-  var groupedData = {};
-
-  routines.forEach(function (routine) {
-    // Extrahera månad och vecka från rutinens klockslag
-    var month = new Date(routine.Klockan).toLocaleString("en-US", {
-      month: "long",
-    });
-    var week = new Date(routine.Klockan).toLocaleString("en-US", {
-      week: "numeric",
-    });
-
-    // Skapa en nyckel för grupperingen baserat på månad och vecka
-    var key = `${month} - Week ${week}`;
-
-    // Om nyckeln inte finns, skapa den
-    if (!groupedData[key]) {
-      groupedData[key] = [];
-    }
-
-    // Lägg till rutinen i rätt grupp
-    groupedData[key].push(routine);
-  });
-
-  return groupedData;
-}
-
-// Funktion för att generera statistik från grupperade data
-function generateStatistics(groupedData) {
-  var statistics = {};
-
-  // Loopa igenom varje grupp (månad och vecka)
-  for (var key in groupedData) {
-    var routinesInGroup = groupedData[key];
-    var totalRoutines = routinesInGroup.length;
-    var completedRoutines = routinesInGroup.filter(
-      (routine) => routine.Done
-    ).length;
-
-    // Skapa statistik för varje grupp
-    statistics[key] = {
-      totalRoutines: totalRoutines,
-      completedRoutines: completedRoutines,
-      completionRate: (completedRoutines / totalRoutines) * 100,
-    };
-  }
-
-  return statistics;
-}
-
 router.get("/", function (req, res, next) {
-  // Ange sökvägarna till mapparna "opening", "closing", "special", "weekly", "monthly" och "summary"
-  var openingFolderPath = path.join(__dirname, "..", "routines", "opening");
-  var closingFolderPath = path.join(__dirname, "..", "routines", "closing");
-  var specialFolderPath = path.join(__dirname, "..", "routines", "special");
-  var weeklyFolderPath = path.join(__dirname, "..", "routines", "weekly");
-  var monthlyFolderPath = path.join(__dirname, "..", "routines", "monthly");
+  // Specify the path to the "summary" folder
   var summaryFolderPath = path.join(__dirname, "..", "routines", "summary");
 
-  // Skapa "summary"-mappen om den inte redan finns
+  // Create the "summary" folder if it doesn't exist
   if (!fs.existsSync(summaryFolderPath)) {
     fs.mkdirSync(summaryFolderPath);
   }
 
   try {
-    // Hämta alla filnamn från olika mappar
-    var openingFiles = fs.readdirSync(openingFolderPath);
-    var closingFiles = fs.readdirSync(closingFolderPath);
-    var specialFiles = fs.readdirSync(specialFolderPath);
-    var weeklyFiles = fs.readdirSync(weeklyFolderPath);
-    var monthlyFiles = fs.readdirSync(monthlyFolderPath);
-
-    // Läs alla rutiner från olika mappar
-    var openingRoutines = openingFiles.map((file) =>
-      readJSONFile(path.join(openingFolderPath, file))
-    );
-    var closingRoutines = closingFiles.map((file) =>
-      readJSONFile(path.join(closingFolderPath, file))
-    );
-    var specialRoutines = specialFiles.map((file) =>
-      readJSONFile(path.join(specialFolderPath, file))
-    );
-    var weeklyRoutines = weeklyFiles.map((file) =>
-      readJSONFile(path.join(weeklyFolderPath, file))
-    );
-    var monthlyRoutines = monthlyFiles.map((file) =>
-      readJSONFile(path.join(monthlyFolderPath, file))
-    );
-
-    // Kombinera alla rutiner
-    var allRoutines = openingRoutines
-      .concat(closingRoutines)
-      .concat(specialRoutines)
-      .concat(weeklyRoutines)
-      .concat(monthlyRoutines);
-
-    // Gruppera rutiner efter månad och vecka
-    var groupedData = groupRoutinesByMonthAndWeek(allRoutines);
-
-    // Generera statistik från grupperade data
-    var statistics = generateStatistics(groupedData);
-
-    // Skriv statistiken till en JSON-fil
+    // Specify the path to the "summary" JSON file
     var statisticsFilePath = path.join(summaryFolderPath, "statistics.json");
-    writeJSONFile(statisticsFilePath, statistics);
+
+    // Read all routines from different folders
+    var allRoutines = [];
+
+    ["opening", "closing", "special", "weekly", "monthly"].forEach((folder) => {
+      var folderPath = path.join(__dirname, "..", "routines", folder);
+      var files = fs.readdirSync(folderPath);
+
+      files.forEach((file) => {
+        var routine = readJSONFile(path.join(folderPath, file));
+        allRoutines.push(routine);
+      });
+    });
+
+    // Organize routines by month > week > day
+    var organizedRoutines = {};
+
+    allRoutines.forEach((routine) => {
+      var month = routine.month; // Replace with the actual property for month
+      var week = routine.week; // Replace with the actual property for week
+
+      if (!organizedRoutines[month]) {
+        organizedRoutines[month] = {};
+      }
+
+      if (!organizedRoutines[month][week]) {
+        organizedRoutines[month][week] = [];
+      }
+
+      organizedRoutines[month][week].push(routine);
+    });
+
+    // Write organized routines to a JSON file
+    writeJSONFile(statisticsFilePath, organizedRoutines);
 
     res.json({
       success: true,
       message: "Routines processed successfully",
-      statistics: statistics,
+      routines: organizedRoutines,
     });
   } catch (error) {
     console.log("Error processing routines", error);
